066ac39770e373f9da350b18233daa42883083bc
dpigasin <dmitry.pigasin@intel.com>
2020-09-21 14:59:07 +0300

    test: add video downloading from aws

--- tests/functional/test_gstreamer.py
+++ tests/functional/test_gstreamer.py
@@ -15,7 +15,7 @@ class TestGstreamerLinux:
         kwargs = {
             'mem_limit': '3g',
             'volumes': {
-                root / 'tests' / 'resources' / 'gst_test': {'bind': '/opt/intel/openvino/gst_test'},
+                root / 'tests' / 'resources' / 'gst_sample': {'bind': '/opt/intel/openvino/gst_sample'},
             },
         }
         tester.test_docker_image(
@@ -31,10 +31,11 @@ class TestGstreamerLinux:
              'python3 -B /opt/intel/openvino/deployment_tools/open_model_zoo/tools/downloader/downloader.py '
              '--name age-gender-recognition-retail-0013 --precisions FP16 -o /tmp/"',
              '/bin/bash -ac ". /opt/intel/openvino/bin/setupvars.sh && '
-             'cd /opt/intel/openvino/gst_test && '
-             'python3 detect_and_classify.py -i Fun_at_a_Fair_without_audio.mp4 '
+             'curl -O https://d30ikxcvcet9xo.cloudfront.net/data/test_data/videos/face-demographics-walking.mp4 && '
+             'cd /opt/intel/openvino/gst_sample && '
+             'python3 detect_and_classify.py -i face-demographics-walking.mp4 '
              '-d /tmp/intel/face-detection-adas-0001/FP16/face-detection-adas-0001.xml '
              '-c /tmp/intel/age-gender-recognition-retail-0013/FP16/age-gender-recognition-retail-0013.xml"'
              ],
-            self.test_gstreamer.__name__, **kwargs,
+            self.test_gstreamer_python.__name__, **kwargs,
         )
