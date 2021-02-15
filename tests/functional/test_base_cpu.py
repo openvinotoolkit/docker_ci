a0474f817d6b6e69f6246754fe1de19cf6ce256d
dpigasin <dmitry.pigasin@intel.com>
2021-02-15 17:11:01 +0300

    refactor: move a dependency installation to test script

--- tests/functional/test_base_cpu.py
+++ tests/functional/test_base_cpu.py
@@ -10,7 +10,7 @@ import pytest
 @pytest.mark.usefixtures('_is_image_os', '_is_distribution')
 @pytest.mark.parametrize('_is_image_os', [('ubuntu18', 'ubuntu20')], indirect=True)
 @pytest.mark.parametrize('_is_distribution', [('base', 'custom-no-cv')], indirect=True)
-def test_base_cpp(tester, image, install_openvino_dependencies):
+def test_base_cpp(tester, image):
     root = pathlib.Path(os.path.realpath(__name__)).parent
     kwargs = {
         'mem_limit': '3g',
@@ -20,7 +20,6 @@ def test_base_cpp(tester, image, install_openvino_dependencies):
     }
     tester.test_docker_image(
         image,
-        [install_openvino_dependencies,
-         '/bin/bash -ac ". /opt/intel/openvino/bin/setupvars.sh && . /opt/intel/openvino/base_cpu/demo.sh"'],
+        ['/bin/bash -ac ". /opt/intel/openvino/bin/setupvars.sh && . /opt/intel/openvino/base_cpu/demo.sh"'],
         test_base_cpp.__name__, **kwargs,
     )
