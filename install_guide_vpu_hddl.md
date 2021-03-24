1ae293c0d586bfd687276e065f55619f76e7933a
Ilya Naumov <ilya.naumov@intel.com>
2021-03-24 18:49:11 +0300

    feat: add 2021.3R links

--- install_guide_vpu_hddl.md
+++ install_guide_vpu_hddl.md
@@ -23,6 +23,7 @@ Available releases for Ubuntu* 18.04:
 *  [2020.4](https://storage.openvinotoolkit.org/drivers/vpu/hddl/2020.4/hddl_ubuntu18_1229.tgz)
 *  [2021.1](https://storage.openvinotoolkit.org/drivers/vpu/hddl/2021.1/hddl_ubuntu18_1380.tgz)
 *  [2021.2](https://storage.openvinotoolkit.org/drivers/vpu/hddl/2021.2/hddl_ubuntu18_1509.tgz)
+*  [2021.3](https://storage.openvinotoolkit.org/drivers/vpu/hddl/2021.3/hddl_ubuntu18_1636.tgz)
 
 
 # Troubleshooting: 
--- utils/loader.py
+++ utils/loader.py
@@ -226,6 +226,49 @@ INTEL_OPENVINO_VERSION = {
                            'l_openvino_toolkit_runtime_rhel8_p_2021.2.185.tgz',
             },
     },
+    '2021.3': {
+        'winserver2019':
+            {
+                'dev': 'https://storage.openvinotoolkit.org/repositories/openvino/packages/2021.3/'
+                       'w_openvino_toolkit_dev_p_2021.3.394.zip',
+                'runtime': 'https://storage.openvinotoolkit.org/repositories/openvino/packages/2021.3/'
+                           'w_openvino_toolkit_runtime_p_2021.3.394.zip',
+                'proprietary': 'https://registrationcenter-download.intel.com/akdlm/irc_nas/17661/'
+                               'w_openvino_toolkit_p_2021.3.394.exe',
+                'data_dev': 'https://storage.openvinotoolkit.org/repositories/openvino/packages/2021.3/'
+                            'w_openvino_toolkit_data_dev_p_2021.3.394.zip',
+            },
+        'ubuntu18':
+            {
+                'dev': 'https://storage.openvinotoolkit.org/repositories/openvino/packages/2021.3/'
+                       'l_openvino_toolkit_dev_ubuntu18_p_2021.3.394.tgz',
+                'runtime': 'https://storage.openvinotoolkit.org/repositories/openvino/packages/2021.3/'
+                           'l_openvino_toolkit_runtime_ubuntu18_p_2021.3.394.tgz',
+                'proprietary': 'http://registrationcenter-download.intel.com/akdlm/irc_nas/17662/'
+                               'l_openvino_toolkit_p_2021.3.394.tgz',
+                'data_dev': 'https://storage.openvinotoolkit.org/repositories/openvino/packages/2021.3/'
+                            'l_openvino_toolkit_data_dev_ubuntu18_p_2021.3.394.tgz',
+            },
+        'ubuntu20':
+            {
+                'dev': 'https://storage.openvinotoolkit.org/repositories/openvino/packages/2021.3/'
+                       'l_openvino_toolkit_dev_ubuntu20_p_2021.3.394.tgz',
+                'runtime': 'https://storage.openvinotoolkit.org/repositories/openvino/packages/2021.3/'
+                           'l_openvino_toolkit_runtime_ubuntu20_p_2021.3.394.tgz',
+                'data_dev': 'https://storage.openvinotoolkit.org/repositories/openvino/packages/2021.3/'
+                            'l_openvino_toolkit_data_dev_ubuntu20_p_2021.3.394.tgz',
+            },
+        'centos7':
+            {
+                'runtime': 'https://storage.openvinotoolkit.org/repositories/openvino/packages/2021.3/'
+                           'l_openvino_toolkit_runtime_centos7_p_2021.3.394.tgz',
+            },
+        'rhel8':
+            {
+                'runtime': 'https://storage.openvinotoolkit.org/repositories/openvino/packages/2021.3/'
+                           'l_openvino_toolkit_runtime_rhel8_p_2021.3.394.tgz',
+            },
+    },
 }
 DIVE_URL = {
     'windows': 'https://github.com/wagoodman/dive/releases/download/v0.9.2/dive_0.9.2_windows_amd64.zip',
