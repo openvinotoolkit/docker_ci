c6632db2d08d20eb71489299e83261171b0186a8
Ilya Naumov <ilya.naumov@intel.com>
2021-08-03 09:57:29 +0300

    build: replace source setupvars.sh with native environment variables (#148)
    * build: set environment variables in dockerfiles
    
    * remove setupvars.sh from templates
    
    * add script to compare path-like variables
    
    * doc: remove outdated info about TF2 venv
    
    * build: cleanup conditions for 2021.4
    
    * test: simplify test_seupvars_changes (compare path-like variables like sets)
    
    * build: add OV_FRONTEND_PATH to env layers

---
+++ tests/functional/test_setupvars_changes.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019-2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+import os
+import pathlib
+
+import pytest
+
+
+@pytest.mark.usefixtures('_is_image_os', '_is_not_distribution')
+@pytest.mark.parametrize('_is_image_os', [('ubuntu18', 'ubuntu20', 'centos7', 'rhel8')], indirect=True)
+@pytest.mark.parametrize('_is_not_distribution', [('base', 'custom-no-omz', 'custom-no-cv',
+                                                   'custom-full')], indirect=True)
+class TestSetupvarsChanges:
+    def test_setupvars_changes_linux(self, tester, image, image_os, distribution):
+        root = pathlib.Path(os.path.realpath(__name__)).parent
+        image_folder = image.replace('/', '_').replace(':', '_')
+        log_folder = root / 'logs' / image_folder / 'environment_vars'
+        if not log_folder.exists():
+            log_folder.mkdir(parents=True)
+        expected_env_file_path = root / 'templates' / image_os / 'env'
+
+        kwargs = {
+            'volumes': {
+                log_folder: {'bind': '/tmp/logs', 'mode': 'rw'},  # nosec # noqa: S108
+                expected_env_file_path: {'bind': '/tmp/dist', 'mode': 'rw'},  # nosec # noqa: S108
+
+                # temp
+                root / 'tests' / 'resources' / 'environment_vars':
+                    {'bind': '/tmp/environment_vars', 'mode': 'rw'},  # nosec # noqa: S108
+            },
+
+        }
+        tester.test_docker_image(
+            image,
+            ["/bin/bash -c 'env > /tmp/logs/env_setupvars_before.txt && "
+             'source /opt/intel/openvino/bin/setupvars.sh > /dev/null && '
+             "env > /tmp/logs/env_setupvars_after.txt'",
+             '/bin/bash -ac "python3 /tmp/environment_vars/env_vars_changes_compare.py '
+             f'-e /tmp/dist/{distribution}_env.dockerfile.j2 '
+             '-b /tmp/logs/env_setupvars_before.txt -a /tmp/logs/env_setupvars_after.txt '
+             f'-i {image} -l /tmp/logs"',
+             ],
+            self.test_setupvars_changes_linux.__name__, **kwargs,
+        )
---
+++ tests/resources/environment_vars/env_vars_changes_compare.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+"""Compare environment variables snapshot with expected and detect changes
+"""
+import argparse
+import difflib
+import logging
+import pathlib
+import re
+import os
+import sys
+import typing
+import uuid
+
+
+def normalize_env_variables(variables: typing.Dict[str, str]) -> typing.Dict[str, str]:
+    """Cleanup environment variables dict from duplicates in PATH-like variables"""
+    output = {}
+    for name, value in variables.items():
+        if name in ['PATH', 'PYTHONPATH', 'PKG_CONFIG_PATH', 'LD_LIBRARY_PATH', 'LIBRARY_PATH', 'OV_FRONTEND_PATH']:
+            paths = set(filter(None, map(str.strip, value.split(':'))))
+            output[name] = ':'.join(sorted(paths))
+        else:
+            output[name] = value
+    return output
+
+
+def extract_changed_environment_variables(vars_before: typing.Dict[str, str],
+                                          vars_after: typing.Dict[str, str]) -> typing.Dict[str, str]:
+    """Extract current values of environment variables (and handle PATH-like variables as set of values)"""
+    return normalize_env_variables(dict(set(vars_after.items()) - set(vars_before.items())))
+
+
+def load_variables(path: str, env_prefix: bool = False) -> typing.Dict[str, str]:
+    """Load environment variables and its values from and env output or a dockerfile-like file"""
+    variables = {}
+    pattern = re.compile(r'^ENV\s+([A-Za-z_]+)=(.*)$' if env_prefix else r'^([A-Za-z_]+)=(.*)$')
+    with open(path) as file:
+        for record in filter(None, map(str.strip, file.readlines())):
+            match = pattern.match(record)
+            if not match:
+                return {}
+            name = match.group(1)
+            value = match.group(2)
+            variables[name] = value
+    return normalize_env_variables(variables)
+
+
+def save_env_template(path: pathlib.Path, variables: typing.Dict[str, str]):
+    """Save environment variables dict in the file in dockerfile-like format"""
+    with open(path, mode='w') as template:
+        for name, value in variables.items():
+            template.write(f'ENV {name}={value}\n')
+
+
+def compare_templates(expected_path: pathlib.Path, actual_path: pathlib.Path, image: str, log: str):
+    """Compare two template files and save HTML diff"""
+    with open(expected_path, mode='r') as expected, \
+         open(actual_path, mode='r') as actual, \
+         open(pathlib.Path(log) / f'env_{uuid.uuid4()}.html', mode='w') as html_log:
+        html_log.write(difflib.HtmlDiff(wrapcolumn=100).make_file(expected.readlines(), actual.readlines(),
+                                                                  'origin', image, context=True))
+
+
+def main() -> int:
+    """Compare environment variables snapshot with expected and create HTML report if different"""
+    parser = argparse.ArgumentParser(prog=os.path.basename(__file__),
+                                     description='This is script to extract environment variables changes from '
+                                                 'snapshots, compare with expected and create HTML diff report '
+                                                 'if different',
+                                     add_help=True)
+    parser.add_argument(
+        '-i',
+        '--image',
+        metavar='NAME',
+        required=True,
+        help='Image name',
+    )
+    parser.add_argument(
+        '-e',
+        '--expected',
+        metavar='PATH',
+        required=True,
+        help='Path to file with expected environment variable changes from the script',
+    )
+    parser.add_argument(
+        '-b',
+        '--before',
+        metavar='PATH',
+        required=True,
+        help='Path to file with environment variables snapshot before script launch',
+    )
+    parser.add_argument(
+        '-a',
+        '--after',
+        metavar='PATH',
+        required=True,
+        help='Path to file with environment variables snapshot after script launch',
+    )
+    parser.add_argument(
+        '-l',
+        '--logs',
+        metavar='PATH',
+        default=str(pathlib.Path(os.path.realpath(__file__)).parent),
+        help='Log path folder to store logs',
+    )
+    args = parser.parse_args()
+    logging.basicConfig(level='INFO')
+    log = logging.getLogger(__name__)
+
+    log.info(f'Parsing inputs...')
+    vars_before = load_variables(args.before)
+    vars_after = load_variables(args.after)
+    vars_created = {name: vars_after[name] for name in set(vars_after.keys()) - set(vars_before.keys())}
+
+    vars_expected = load_variables(args.expected, True)
+    vars_expected_updated = {name: vars_after[name] for name in vars_after if name in vars_expected}
+    vars_current = {**vars_expected, **vars_created, **vars_expected_updated}
+
+    log.info('Generate updated environment variables template and search for changes:')
+    output_path = pathlib.Path(args.logs) / os.path.basename(args.expected)
+    save_env_template(output_path, vars_current)
+    if vars_expected != vars_current:
+        exit_code = 1
+        vars_changed_script = extract_changed_environment_variables(vars_before, vars_after)
+        vars_changed = extract_changed_environment_variables(vars_expected, vars_current)
+        log.error('FAILED: changes detected')
+        log.error(f'    after script launch {vars_changed_script}')
+        log.error(f'    with expected {vars_changed}')
+        compare_templates(args.expected, output_path, args.image, args.logs)
+    else:
+        exit_code = 0
+        log.info('PASSED')
+    if vars_created:
+        exit_code = 1
+        log.error(f'FAILED: new variables are created - {vars_created}')
+
+    if exit_code:
+        log.info(f'See logs in {args.logs}')
+
+    return exit_code
+
+
+if __name__ == '__main__':
+    sys.exit(main())
