1682054856c936af94d73303e54a885c0182e543
Kate Generalova <kate.generalova@intel.com>
2021-08-23 16:06:20 +0300

    test: check 3d party file exist (#163)
    * test: add test on 3d party txt
    
    * fix: missing fixture
    
    * fix: missing linux fixture
    
    * fix: missing linux fixture
    
    * test: check distribution for win 3d party test

--- tests/functional/test_package_licenses.py
+++ tests/functional/test_package_licenses.py
@@ -7,19 +7,53 @@ import pathlib
 import pytest
 
 
-@pytest.mark.usefixtures('_is_image_os')
-@pytest.mark.parametrize('_is_image_os', [('ubuntu18', 'ubuntu20')], indirect=True)
-def test_package_licenses(tester, image):
-    root = pathlib.Path(os.path.realpath(__name__)).parent
-    image_folder = image.replace('/', '_').replace(':', '_')
-    kwargs = {
-        'volumes': {
-            root / 'logs' / image_folder / 'linux_deps': {'bind': '/tmp/logs'},  # nosec # noqa: S108
-            root / 'tests' / 'resources' / 'linux_deps': {'bind': '/tmp/linux_deps'},  # nosec # noqa: S108
-        },
-    }
-    tester.test_docker_image(
-        image,
-        ['/bin/bash -ac "/tmp/linux_deps/package_licenses.sh"'],
-        test_package_licenses.__name__, **kwargs,
-    )
+@pytest.mark.usefixtures('_is_not_image_os')
+@pytest.mark.parametrize('_is_not_image_os', [('winserver2019', 'windows20h2')], indirect=True)
+class TestLicenseLinux:
+    @pytest.mark.usefixtures('_is_image_os')
+    @pytest.mark.parametrize('_is_image_os', [('ubuntu18', 'ubuntu20')], indirect=True)
+    def test_package_licenses(self, tester, image):
+        root = pathlib.Path(os.path.realpath(__name__)).parent
+        image_folder = image.replace('/', '_').replace(':', '_')
+        kwargs = {
+            'volumes': {
+                root / 'logs' / image_folder / 'linux_deps': {'bind': '/tmp/logs'},  # nosec # noqa: S108
+                root / 'tests' / 'resources' / 'linux_deps': {'bind': '/tmp/linux_deps'},  # nosec # noqa: S108
+            },
+        }
+        tester.test_docker_image(
+            image,
+            ['/bin/bash -ac "/tmp/linux_deps/package_licenses.sh"'],
+            self.test_package_licenses.__name__, **kwargs,
+        )
+
+    @pytest.mark.usefixtures('_is_distribution')
+    @pytest.mark.parametrize('_is_distribution', [('dev', 'data_dev', 'dev_no_samples', 'proprietary')], indirect=True)
+    def test_3d_party_dev_lin(self, tester, image):
+        tester.test_docker_image(
+            image,
+            ['/bin/bash -ac "test -f ./licensing/third-party-programs-docker-dev.txt"'],
+            self.test_3d_party_dev_lin.__name__,
+        )
+
+    @pytest.mark.usefixtures('_is_distribution')
+    @pytest.mark.parametrize('_is_distribution', [('runtime', 'data_runtime')], indirect=True)
+    def test_3d_party_runtime_lin(self, tester, image):
+        tester.test_docker_image(
+            image,
+            ['/bin/bash -ac "test -f ./licensing/third-party-programs-docker-runtime.txt"'],
+            self.test_3d_party_runtime_lin.__name__,
+        )
+
+
+@pytest.mark.usefixtures('_is_image_os', '_is_distribution')
+@pytest.mark.parametrize('_is_image_os', [('winserver2019', 'windows20h2')], indirect=True)
+@pytest.mark.parametrize('_is_distribution', [('runtime')], indirect=True)
+class TestLicenseWindows:
+    def test_3d_party_runtime_win(self, tester, image):
+        kwargs = {'user': 'ContainerAdministrator'}
+        tester.test_docker_image(
+            image,
+            ['cmd /S /C type C:\\\\intel\\\\openvino\\\\licensing\\\\third-party-programs-docker-runtime.txt'],
+            self.test_3d_party_runtime_win.__name__, **kwargs,
+        )
