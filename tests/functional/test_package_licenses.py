48140ae361f015e9c437e85816fa1c5e21434f4d
Kate Generalova <kate.generalova@intel.com>
2021-08-23 14:32:27 +0300

    fix: missing linux fixture

--- tests/functional/test_package_licenses.py
+++ tests/functional/test_package_licenses.py
@@ -7,7 +7,7 @@ import pathlib
 import pytest
 
 
-@pytest.mark.usefixtures('_is_not_image_os', '_is_distribution')
+@pytest.mark.usefixtures('_is_not_image_os', '_is_image_os', '_is_distribution')
 @pytest.mark.parametrize('_is_not_image_os', [('winserver2019', 'windows20h2')], indirect=True)
 class TestLicenseLinux:
     @pytest.mark.parametrize('_is_image_os', [('ubuntu18', 'ubuntu20')], indirect=True)
