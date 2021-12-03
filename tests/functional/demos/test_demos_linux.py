2ecb68e07949e3860fe2e6b57ad938b2d86e56b2
Ilya Naumov <ilya.naumov@intel.com>
2021-12-03 17:00:20 +0300

    feat: improvements for RHEL images (#197)
    * build: fix "find_namespace: does not exist" error
    
    * test: enable sample script tests on rhel
    
    * feat: generate dockerfiles for Red Hat portal Build System (#185)
    
    * build: add --rhel_platform argument to argparser
    
    set --openshift as alias for --rhel_platform==openshift
    
    * build: add rhel autobuild dockerfiles generation
    
    * build: move label to the beginning of non-docker dockerfiles
    
    * test: add benchmark script, move script tests to samples

--- tests/functional/demos/test_demos_linux.py
+++ tests/functional/demos/test_demos_linux.py
@@ -106,157 +106,6 @@ class TestSpeechDemoLinux:
 
 
 @pytest.mark.usefixtures('_is_image_os', '_is_distribution')
-@pytest.mark.parametrize('_is_image_os', [('ubuntu18', 'ubuntu20')], indirect=True)
-@pytest.mark.parametrize('_is_distribution', [('dev', 'proprietary', 'custom-full')], indirect=True)
-class TestScriptDemosLinux:
-    def test_security_cpu(self, tester, image, install_openvino_dependencies, bash, download_picture):
-        tester.test_docker_image(
-            image,
-            [install_openvino_dependencies,
-             bash('/opt/intel/openvino/extras/open_model_zoo/demos/build_demos.sh'),
-             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
-                  '--name vehicle-license-plate-detection-barrier-0106 -o /root/omz_demos_build/intel64/Release/'),
-             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
-                  '--name license-plate-recognition-barrier-0001 -o /root/omz_demos_build/intel64/Release/'),
-             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
-                  '--name vehicle-attributes-recognition-barrier-0039 -o /root/omz_demos_build/intel64/Release/'),
-             download_picture('car_1.bmp'),
-             bash('/root/omz_demos_build/intel64/Release/security_barrier_camera_demo '
-                  '-i /opt/intel/openvino/samples/scripts/car_1.bmp '
-                  '-m /root/omz_demos_build/intel64/Release/intel/vehicle-license-plate-detection-barrier-0106/'
-                  'FP16/vehicle-license-plate-detection-barrier-0106.xml '
-                  '-m_lpr /root/omz_demos_build/intel64/Release/intel/license-plate-recognition-barrier-0001/'
-                  'FP16/license-plate-recognition-barrier-0001.xml '
-                  '-m_va /root/omz_demos_build/intel64/Release/intel/vehicle-attributes-recognition-barrier-0039/'
-                  'FP16/vehicle-attributes-recognition-barrier-0039.xml -no_show -d CPU -d_va CPU -d_lpr CPU'),
-             ], self.test_security_cpu.__name__,
-        )
-
-    @pytest.mark.gpu
-    def test_security_gpu(self, tester, image, install_openvino_dependencies, bash, download_picture):
-        kwargs = {'devices': ['/dev/dri:/dev/dri'], 'mem_limit': '3g'}
-        tester.test_docker_image(
-            image,
-            [install_openvino_dependencies,
-             bash('/opt/intel/openvino/extras/open_model_zoo/demos/build_demos.sh'),
-             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
-                  '--name vehicle-license-plate-detection-barrier-0106 -o /root/omz_demos_build/intel64/Release/'),
-             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
-                  '--name license-plate-recognition-barrier-0001 -o /root/omz_demos_build/intel64/Release/'),
-             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
-                  '--name vehicle-attributes-recognition-barrier-0039 -o /root/omz_demos_build/intel64/Release/'),
-             download_picture('car_1.bmp'),
-             bash('/root/omz_demos_build/intel64/Release/security_barrier_camera_demo '
-                  '-i /opt/intel/openvino/samples/scripts/car_1.bmp '
-                  '-m /root/omz_demos_build/intel64/Release/intel/vehicle-license-plate-detection-barrier-0106/'
-                  'FP16/vehicle-license-plate-detection-barrier-0106.xml '
-                  '-m_lpr /root/omz_demos_build/intel64/Release/intel/license-plate-recognition-barrier-0001/'
-                  'FP16/license-plate-recognition-barrier-0001.xml '
-                  '-m_va /root/omz_demos_build/intel64/Release/intel/vehicle-attributes-recognition-barrier-0039/'
-                  'FP16/vehicle-attributes-recognition-barrier-0039.xml -no_show -d GPU -d_va GPU -d_lpr GPU'),
-             ], self.test_security_gpu.__name__, **kwargs,
-        )
-
-    @pytest.mark.vpu
-    @pytest.mark.xfail_log(pattern='Can not init Myriad device: NC_ERROR',
-                           reason='Sporadic error on MYRIAD device')
-    def test_security_vpu(self, tester, image, install_openvino_dependencies, bash, download_picture):
-        kwargs = {'device_cgroup_rules': ['c 189:* rmw'],
-                  'volumes': ['/dev/bus/usb:/dev/bus/usb'], 'mem_limit': '3g'}  # nosec # noqa: S108
-        tester.test_docker_image(
-            image,
-            [install_openvino_dependencies,
-             bash('/opt/intel/openvino/extras/open_model_zoo/demos/build_demos.sh'),
-             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
-                  '--name vehicle-license-plate-detection-barrier-0106 -o /root/omz_demos_build/intel64/Release/'),
-             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
-                  '--name license-plate-recognition-barrier-0001 -o /root/omz_demos_build/intel64/Release/'),
-             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
-                  '--name vehicle-attributes-recognition-barrier-0039 -o /root/omz_demos_build/intel64/Release/'),
-             download_picture('car_1.bmp'),
-             bash('/root/omz_demos_build/intel64/Release/security_barrier_camera_demo '
-                  '-i /opt/intel/openvino/samples/scripts/car_1.bmp '
-                  '-m /root/omz_demos_build/intel64/Release/intel/vehicle-license-plate-detection-barrier-0106/'
-                  'FP16/vehicle-license-plate-detection-barrier-0106.xml '
-                  '-m_lpr /root/omz_demos_build/intel64/Release/intel/license-plate-recognition-barrier-0001/'
-                  'FP16/license-plate-recognition-barrier-0001.xml '
-                  '-m_va /root/omz_demos_build/intel64/Release/intel/vehicle-attributes-recognition-barrier-0039/'
-                  'FP16/vehicle-attributes-recognition-barrier-0039.xml -no_show -d MYRIAD -d_va MYRIAD -d_lpr MYRIAD'),
-             ], self.test_security_vpu.__name__, **kwargs,
-        )
-
-    @pytest.mark.hddl
-    def test_security_hddl(self, tester, image, install_openvino_dependencies, bash, download_picture):
-        kwargs = {'devices': ['/dev/ion:/dev/ion'],
-                  'volumes': ['/var/tmp:/var/tmp', '/dev/shm:/dev/shm'], 'mem_limit': '3g'}  # nosec # noqa: S108
-        tester.test_docker_image(
-            image,
-            [install_openvino_dependencies,
-             bash('/opt/intel/openvino/extras/open_model_zoo/demos/build_demos.sh'),
-             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
-                  '--name vehicle-license-plate-detection-barrier-0106 -o /root/omz_demos_build/intel64/Release/'),
-             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
-                  '--name license-plate-recognition-barrier-0001 -o /root/omz_demos_build/intel64/Release/'),
-             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
-                  '--name vehicle-attributes-recognition-barrier-0039 -o /root/omz_demos_build/intel64/Release/'),
-             download_picture('car_1.bmp'),
-             bash('umask 0000 && /root/omz_demos_build/intel64/Release/security_barrier_camera_demo '
-                  '-i /opt/intel/openvino/samples/scripts/car_1.bmp '
-                  '-m /root/omz_demos_build/intel64/Release/intel/vehicle-license-plate-detection-barrier-0106/'
-                  'FP16/vehicle-license-plate-detection-barrier-0106.xml '
-                  '-m_lpr /root/omz_demos_build/intel64/Release/intel/license-plate-recognition-barrier-0001/'
-                  'FP16/license-plate-recognition-barrier-0001.xml '
-                  '-m_va /root/omz_demos_build/intel64/Release/intel/vehicle-attributes-recognition-barrier-0039/'
-                  'FP16/vehicle-attributes-recognition-barrier-0039.xml -no_show -d HDDL -d_va HDDL -d_lpr HDDL '
-                  '-d HDDL -sample-options -no_show && rm -f /dev/shm/hddl_*"'),
-             ], self.test_security_hddl.__name__, **kwargs,
-        )
-
-    def test_squeezenet_cpu(self, tester, image, install_openvino_dependencies):
-        tester.test_docker_image(
-            image,
-            [install_openvino_dependencies,
-             '/opt/intel/openvino/samples/scripts/run_sample_squeezenet.sh -d CPU',
-             ], self.test_squeezenet_cpu.__name__,
-        )
-
-    @pytest.mark.gpu
-    def test_squeezenet_gpu(self, tester, image, install_openvino_dependencies):
-        kwargs = {'devices': ['/dev/dri:/dev/dri'], 'mem_limit': '3g'}
-        tester.test_docker_image(
-            image,
-            [install_openvino_dependencies,
-             '/opt/intel/openvino/samples/scripts/run_sample_squeezenet.sh -d GPU',
-             ], self.test_squeezenet_gpu.__name__, **kwargs,
-        )
-
-    @pytest.mark.vpu
-    @pytest.mark.xfail_log(pattern='Can not init Myriad device: NC_ERROR',
-                           reason='Sporadic error on MYRIAD device')
-    def test_squeezenet_vpu(self, tester, image, install_openvino_dependencies):
-        kwargs = {'device_cgroup_rules': ['c 189:* rmw'],
-                  'volumes': ['/dev/bus/usb:/dev/bus/usb'], 'mem_limit': '3g'}  # nosec # noqa: S108
-        tester.test_docker_image(
-            image,
-            [install_openvino_dependencies,
-             '/opt/intel/openvino/samples/scripts/run_sample_squeezenet.sh -d MYRIAD',
-             ], self.test_squeezenet_vpu.__name__, **kwargs,
-        )
-
-    @pytest.mark.hddl
-    def test_squeezenet_hddl(self, tester, image, install_openvino_dependencies, bash):
-        kwargs = {'devices': ['/dev/ion:/dev/ion'],
-                  'volumes': ['/var/tmp:/var/tmp', '/dev/shm:/dev/shm'], 'mem_limit': '3g'}  # nosec # noqa: S108
-        tester.test_docker_image(
-            image,
-            [install_openvino_dependencies,
-             bash('umask 0000 && /opt/intel/openvino/samples/scripts/run_sample_squeezenet.sh '
-                  '-d HDDL && rm -f /dev/shm/hddl_*'),
-             ], self.test_squeezenet_hddl.__name__, **kwargs,
-        )
-
-
-@pytest.mark.usefixtures('_is_image_os', '_is_distribution')
 @pytest.mark.parametrize('_is_image_os', [('ubuntu18', 'ubuntu20', 'rhel8')], indirect=True)
 @pytest.mark.parametrize('_is_distribution', [('dev', 'proprietary', 'custom-full')], indirect=True)
 class TestDemosLinux:
@@ -337,6 +186,113 @@ class TestDemosLinux:
                   '-i /opt/intel/openvino/samples/scripts/car_1.bmp -d HDDL -no_show && rm -f /dev/shm/hddl_*'),
              ],
             self.test_crossroad_cpp_hddl.__name__, **kwargs,
+        )
+
+    def test_security_cpu(self, tester, image, install_openvino_dependencies, bash, download_picture):
+        tester.test_docker_image(
+            image,
+            [install_openvino_dependencies,
+             bash('/opt/intel/openvino/extras/open_model_zoo/demos/build_demos.sh'),
+             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
+                  '--name vehicle-license-plate-detection-barrier-0106 -o /root/omz_demos_build/intel64/Release/'),
+             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
+                  '--name license-plate-recognition-barrier-0001 -o /root/omz_demos_build/intel64/Release/'),
+             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
+                  '--name vehicle-attributes-recognition-barrier-0039 -o /root/omz_demos_build/intel64/Release/'),
+             download_picture('car_1.bmp'),
+             bash('/root/omz_demos_build/intel64/Release/security_barrier_camera_demo '
+                  '-i /opt/intel/openvino/samples/scripts/car_1.bmp '
+                  '-m /root/omz_demos_build/intel64/Release/intel/vehicle-license-plate-detection-barrier-0106/'
+                  'FP16/vehicle-license-plate-detection-barrier-0106.xml '
+                  '-m_lpr /root/omz_demos_build/intel64/Release/intel/license-plate-recognition-barrier-0001/'
+                  'FP16/license-plate-recognition-barrier-0001.xml '
+                  '-m_va /root/omz_demos_build/intel64/Release/intel/vehicle-attributes-recognition-barrier-0039/'
+                  'FP16/vehicle-attributes-recognition-barrier-0039.xml -no_show -d CPU -d_va CPU -d_lpr CPU'),
+             ], self.test_security_cpu.__name__,
+        )
+
+    @pytest.mark.gpu
+    def test_security_gpu(self, tester, image, install_openvino_dependencies, bash, download_picture):
+        kwargs = {'devices': ['/dev/dri:/dev/dri'], 'mem_limit': '3g'}
+        tester.test_docker_image(
+            image,
+            [install_openvino_dependencies,
+             bash('/opt/intel/openvino/extras/open_model_zoo/demos/build_demos.sh'),
+             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
+                  '--name vehicle-license-plate-detection-barrier-0106 -o /root/omz_demos_build/intel64/Release/'),
+             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
+                  '--name license-plate-recognition-barrier-0001 -o /root/omz_demos_build/intel64/Release/'),
+             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
+                  '--name vehicle-attributes-recognition-barrier-0039 -o /root/omz_demos_build/intel64/Release/'),
+             download_picture('car_1.bmp'),
+             bash('/root/omz_demos_build/intel64/Release/security_barrier_camera_demo '
+                  '-i /opt/intel/openvino/samples/scripts/car_1.bmp '
+                  '-m /root/omz_demos_build/intel64/Release/intel/vehicle-license-plate-detection-barrier-0106/'
+                  'FP16/vehicle-license-plate-detection-barrier-0106.xml '
+                  '-m_lpr /root/omz_demos_build/intel64/Release/intel/license-plate-recognition-barrier-0001/'
+                  'FP16/license-plate-recognition-barrier-0001.xml '
+                  '-m_va /root/omz_demos_build/intel64/Release/intel/vehicle-attributes-recognition-barrier-0039/'
+                  'FP16/vehicle-attributes-recognition-barrier-0039.xml -no_show -d GPU -d_va GPU -d_lpr GPU'),
+             ], self.test_security_gpu.__name__, **kwargs,
+        )
+
+    @pytest.mark.vpu
+    @pytest.mark.xfail_log(pattern='Can not init Myriad device: NC_ERROR',
+                           reason='Sporadic error on MYRIAD device')
+    @pytest.mark.usefixtures('_is_not_image_os')
+    @pytest.mark.parametrize('_is_not_image_os', [('rhel8')], indirect=True)
+    def test_security_vpu(self, tester, image, install_openvino_dependencies, bash, download_picture):
+        kwargs = {'device_cgroup_rules': ['c 189:* rmw'],
+                  'volumes': ['/dev/bus/usb:/dev/bus/usb'], 'mem_limit': '3g'}  # nosec # noqa: S108
+        tester.test_docker_image(
+            image,
+            [install_openvino_dependencies,
+             bash('/opt/intel/openvino/extras/open_model_zoo/demos/build_demos.sh'),
+             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
+                  '--name vehicle-license-plate-detection-barrier-0106 -o /root/omz_demos_build/intel64/Release/'),
+             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
+                  '--name license-plate-recognition-barrier-0001 -o /root/omz_demos_build/intel64/Release/'),
+             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
+                  '--name vehicle-attributes-recognition-barrier-0039 -o /root/omz_demos_build/intel64/Release/'),
+             download_picture('car_1.bmp'),
+             bash('/root/omz_demos_build/intel64/Release/security_barrier_camera_demo '
+                  '-i /opt/intel/openvino/samples/scripts/car_1.bmp '
+                  '-m /root/omz_demos_build/intel64/Release/intel/vehicle-license-plate-detection-barrier-0106/'
+                  'FP16/vehicle-license-plate-detection-barrier-0106.xml '
+                  '-m_lpr /root/omz_demos_build/intel64/Release/intel/license-plate-recognition-barrier-0001/'
+                  'FP16/license-plate-recognition-barrier-0001.xml '
+                  '-m_va /root/omz_demos_build/intel64/Release/intel/vehicle-attributes-recognition-barrier-0039/'
+                  'FP16/vehicle-attributes-recognition-barrier-0039.xml -no_show -d MYRIAD -d_va MYRIAD -d_lpr MYRIAD'),
+             ], self.test_security_vpu.__name__, **kwargs,
+        )
+
+    @pytest.mark.hddl
+    @pytest.mark.usefixtures('_is_not_image_os')
+    @pytest.mark.parametrize('_is_not_image_os', [('rhel8')], indirect=True)
+    def test_security_hddl(self, tester, image, install_openvino_dependencies, bash, download_picture):
+        kwargs = {'devices': ['/dev/ion:/dev/ion'],
+                  'volumes': ['/var/tmp:/var/tmp', '/dev/shm:/dev/shm'], 'mem_limit': '3g'}  # nosec # noqa: S108
+        tester.test_docker_image(
+            image,
+            [install_openvino_dependencies,
+             bash('/opt/intel/openvino/extras/open_model_zoo/demos/build_demos.sh'),
+             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
+                  '--name vehicle-license-plate-detection-barrier-0106 -o /root/omz_demos_build/intel64/Release/'),
+             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
+                  '--name license-plate-recognition-barrier-0001 -o /root/omz_demos_build/intel64/Release/'),
+             bash('python3 /opt/intel/openvino/extras/open_model_zoo/tools/model_tools/downloader.py '
+                  '--name vehicle-attributes-recognition-barrier-0039 -o /root/omz_demos_build/intel64/Release/'),
+             download_picture('car_1.bmp'),
+             bash('umask 0000 && /root/omz_demos_build/intel64/Release/security_barrier_camera_demo '
+                  '-i /opt/intel/openvino/samples/scripts/car_1.bmp '
+                  '-m /root/omz_demos_build/intel64/Release/intel/vehicle-license-plate-detection-barrier-0106/'
+                  'FP16/vehicle-license-plate-detection-barrier-0106.xml '
+                  '-m_lpr /root/omz_demos_build/intel64/Release/intel/license-plate-recognition-barrier-0001/'
+                  'FP16/license-plate-recognition-barrier-0001.xml '
+                  '-m_va /root/omz_demos_build/intel64/Release/intel/vehicle-attributes-recognition-barrier-0039/'
+                  'FP16/vehicle-attributes-recognition-barrier-0039.xml -no_show -d HDDL -d_va HDDL -d_lpr HDDL '
+                  '-d HDDL -sample-options -no_show && rm -f /dev/shm/hddl_*"'),
+             ], self.test_security_hddl.__name__, **kwargs,
         )
 
     def test_text_cpp_cpu(self, tester, image, install_openvino_dependencies, bash, download_picture):
