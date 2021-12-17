d400d001767fd15cc6b8fdb3f2cc5b95e55ea5e5
Ilya Naumov <ilya.naumov@intel.com>
2021-12-17 11:10:51 +0300

    test: fix benchmark_app tests

--- tests/functional/samples/test_samples_linux.py
+++ tests/functional/samples/test_samples_linux.py
@@ -9,106 +9,90 @@ from utils.exceptions import FailedTestError
 @pytest.mark.usefixtures('_is_image_os', '_is_distribution')
 @pytest.mark.parametrize('_is_image_os', [('ubuntu18', 'ubuntu20', 'rhel8')], indirect=True)
 @pytest.mark.parametrize('_is_distribution', [('dev', 'proprietary', 'custom-full')], indirect=True)
-class TestSampleScriptsLinux:
-    def test_benchmark_app_cpu(self, tester, image, install_openvino_dependencies):
-        tester.test_docker_image(
-            image,
-            [install_openvino_dependencies,
-             '/opt/intel/openvino/samples/scripts/run_sample_benchmark_app.sh -d CPU',
-             ], self.test_benchmark_app_cpu.__name__,
-        )
-
-    @pytest.mark.gpu
-    def test_benchmark_app_gpu(self, tester, image, install_openvino_dependencies):
-        kwargs = {'devices': ['/dev/dri:/dev/dri'], 'mem_limit': '3g'}
-        tester.test_docker_image(
-            image,
-            [install_openvino_dependencies,
-             '/opt/intel/openvino/samples/scripts/run_sample_benchmark_app.sh -d GPU',
-             ], self.test_benchmark_app_gpu.__name__, **kwargs,
-        )
-
-    @pytest.mark.vpu
-    @pytest.mark.xfail_log(pattern='Can not init Myriad device: NC_ERROR',
-                           reason='Sporadic error on MYRIAD device')
-    @pytest.mark.usefixtures('_is_not_image_os')
-    @pytest.mark.parametrize('_is_not_image_os', [('rhel8')], indirect=True)
-    def test_benchmark_app_vpu(self, tester, image, install_openvino_dependencies):
-        kwargs = {'device_cgroup_rules': ['c 189:* rmw'],
-                  'volumes': ['/dev/bus/usb:/dev/bus/usb'], 'mem_limit': '3g'}  # nosec # noqa: S108
-        tester.test_docker_image(
-            image,
-            [install_openvino_dependencies,
-             '/opt/intel/openvino/samples/scripts/run_sample_benchmark_app.sh -d MYRIAD',
-             ], self.test_benchmark_app_vpu.__name__, **kwargs,
-        )
-
-    @pytest.mark.hddl
-    @pytest.mark.usefixtures('_is_not_image_os')
-    @pytest.mark.parametrize('_is_not_image_os', [('rhel8')], indirect=True)
-    def test_benchmark_app_hddl(self, tester, image, install_openvino_dependencies, bash):
-        kwargs = {'devices': ['/dev/ion:/dev/ion'],
-                  'volumes': ['/var/tmp:/var/tmp', '/dev/shm:/dev/shm'], 'mem_limit': '3g'}  # nosec # noqa: S108
-        tester.test_docker_image(
-            image,
-            [install_openvino_dependencies,
-             bash('umask 0000 && /opt/intel/openvino/samples/scripts/run_sample_benchmark_app.sh '
-                  '-d HDDL && rm -f /dev/shm/hddl_*'),
-             ], self.test_benchmark_app_hddl.__name__, **kwargs,
-        )
-
-    def test_squeezenet_cpu(self, tester, image, install_openvino_dependencies):
-        tester.test_docker_image(
-            image,
-            [install_openvino_dependencies,
-             '/opt/intel/openvino/samples/scripts/run_sample_squeezenet.sh -d CPU',
-             ], self.test_squeezenet_cpu.__name__,
-        )
-
-    @pytest.mark.gpu
-    def test_squeezenet_gpu(self, tester, image, install_openvino_dependencies):
-        kwargs = {'devices': ['/dev/dri:/dev/dri'], 'mem_limit': '3g'}
-        tester.test_docker_image(
-            image,
-            [install_openvino_dependencies,
-             '/opt/intel/openvino/samples/scripts/run_sample_squeezenet.sh -d GPU',
-             ], self.test_squeezenet_gpu.__name__, **kwargs,
-        )
-
-    @pytest.mark.vpu
-    @pytest.mark.xfail_log(pattern='Can not init Myriad device: NC_ERROR',
-                           reason='Sporadic error on MYRIAD device')
-    @pytest.mark.usefixtures('_is_not_image_os')
-    @pytest.mark.parametrize('_is_not_image_os', [('rhel8')], indirect=True)
-    def test_squeezenet_vpu(self, tester, image, install_openvino_dependencies):
-        kwargs = {'device_cgroup_rules': ['c 189:* rmw'],
-                  'volumes': ['/dev/bus/usb:/dev/bus/usb'], 'mem_limit': '3g'}  # nosec # noqa: S108
-        tester.test_docker_image(
-            image,
-            [install_openvino_dependencies,
-             '/opt/intel/openvino/samples/scripts/run_sample_squeezenet.sh -d MYRIAD',
-             ], self.test_squeezenet_vpu.__name__, **kwargs,
-        )
-
-    @pytest.mark.hddl
-    @pytest.mark.usefixtures('_is_not_image_os')
-    @pytest.mark.parametrize('_is_not_image_os', [('rhel8')], indirect=True)
-    def test_squeezenet_hddl(self, tester, image, install_openvino_dependencies, bash):
-        kwargs = {'devices': ['/dev/ion:/dev/ion'],
-                  'volumes': ['/var/tmp:/var/tmp', '/dev/shm:/dev/shm'], 'mem_limit': '3g'}  # nosec # noqa: S108
-        tester.test_docker_image(
-            image,
-            [install_openvino_dependencies,
-             bash('umask 0000 && /opt/intel/openvino/samples/scripts/run_sample_squeezenet.sh '
-                  '-d HDDL && rm -f /dev/shm/hddl_*'),
-             ], self.test_squeezenet_hddl.__name__, **kwargs,
-        )
-
-
-@pytest.mark.usefixtures('_is_image_os', '_is_distribution')
-@pytest.mark.parametrize('_is_image_os', [('ubuntu18', 'ubuntu20', 'rhel8')], indirect=True)
-@pytest.mark.parametrize('_is_distribution', [('dev', 'proprietary', 'custom-full')], indirect=True)
 class TestSamplesLinux:
+    def test_benchmark_app_cpp_cpu(self, tester, image, install_openvino_dependencies, bash, download_picture):
+        tester.test_docker_image(
+            image,
+            [install_openvino_dependencies,
+             bash('cd /opt/intel/openvino/samples/cpp && '
+                  '/opt/intel/openvino/samples/cpp/build_samples.sh'),
+             bash('omz_downloader --name squeezenet1.1 --precisions FP16 '
+                  '-o /root/inference_engine_cpp_samples_build/intel64/Release/'),
+             bash('omz_converter --name squeezenet1.1 --precisions FP16 '
+                  '-d /root/inference_engine_cpp_samples_build/intel64/Release/'),
+             download_picture('car.png'),
+             bash('/root/inference_engine_cpp_samples_build/intel64/Release/benchmark_app -pc -niter 1000 -m '
+                  '/root/inference_engine_cpp_samples_build/intel64/Release/public/squeezenet1.1/FP16/squeezenet1.1.xml'
+                  ' -i /opt/intel/openvino/samples/scripts/car.png -d CPU'),
+             ], self.test_benchmark_app_cpp_cpu.__name__,
+        )
+
+    @pytest.mark.gpu
+    def test_benchmark_app_cpp_gpu(self, tester, image, install_openvino_dependencies, bash, download_picture):
+        kwargs = {'devices': ['/dev/dri:/dev/dri'], 'mem_limit': '3g'}
+        tester.test_docker_image(
+            image,
+            [install_openvino_dependencies,
+             bash('cd /opt/intel/openvino/samples/cpp && '
+                  '/opt/intel/openvino/samples/cpp/build_samples.sh'),
+             bash('omz_downloader --name squeezenet1.1 --precisions FP16 '
+                  '-o /root/inference_engine_cpp_samples_build/intel64/Release/'),
+             bash('omz_converter --name squeezenet1.1 --precisions FP16 '
+                  '-d /root/inference_engine_cpp_samples_build/intel64/Release/'),
+             download_picture('car.png'),
+             bash('/root/inference_engine_cpp_samples_build/intel64/Release/benchmark_app -pc -niter 1000 -m '
+                  '/root/inference_engine_cpp_samples_build/intel64/Release/public/squeezenet1.1/FP16/squeezenet1.1.xml'
+                  ' -i /opt/intel/openvino/samples/scripts/car.png -d GPU'),
+             ], self.test_benchmark_app_cpp_gpu.__name__, **kwargs,
+        )
+
+    @pytest.mark.vpu
+    @pytest.mark.xfail_log(pattern='Can not init Myriad device: NC_ERROR',
+                           reason='Sporadic error on MYRIAD device')
+    @pytest.mark.usefixtures('_is_not_image_os')
+    @pytest.mark.parametrize('_is_not_image_os', [('rhel8')], indirect=True)
+    def test_benchmark_app_cpp_vpu(self, tester, image, install_openvino_dependencies, bash, download_picture):
+        kwargs = {'device_cgroup_rules': ['c 189:* rmw'],
+                  'volumes': ['/dev/bus/usb:/dev/bus/usb'], 'mem_limit': '3g'}  # nosec # noqa: S108
+        tester.test_docker_image(
+            image,
+            [install_openvino_dependencies,
+             bash('cd /opt/intel/openvino/samples/cpp && '
+                  '/opt/intel/openvino/samples/cpp/build_samples.sh'),
+             bash('omz_downloader --name squeezenet1.1 --precisions FP16 '
+                  '-o /root/inference_engine_cpp_samples_build/intel64/Release/'),
+             bash('omz_converter --name squeezenet1.1 --precisions FP16 '
+                  '-d /root/inference_engine_cpp_samples_build/intel64/Release/'),
+             download_picture('car.png'),
+             bash('/root/inference_engine_cpp_samples_build/intel64/Release/benchmark_app -pc -niter 1000 -m '
+                  '/root/inference_engine_cpp_samples_build/intel64/Release/public/squeezenet1.1/FP16/squeezenet1.1.xml'
+                  ' -i /opt/intel/openvino/samples/scripts/car.png -d MYRIAD'),
+             ], self.test_benchmark_app_cpp_vpu.__name__, **kwargs,
+        )
+
+    @pytest.mark.hddl
+    @pytest.mark.usefixtures('_is_not_image_os')
+    @pytest.mark.parametrize('_is_not_image_os', [('rhel8')], indirect=True)
+    def test_benchmark_app_cpp_hddl(self, tester, image, install_openvino_dependencies, bash, download_picture):
+        kwargs = {'devices': ['/dev/ion:/dev/ion'],
+                  'volumes': ['/var/tmp:/var/tmp', '/dev/shm:/dev/shm'], 'mem_limit': '3g'}  # nosec # noqa: S108
+        tester.test_docker_image(
+            image,
+            [install_openvino_dependencies,
+             bash('cd /opt/intel/openvino/samples/cpp && '
+                  '/opt/intel/openvino/samples/cpp/build_samples.sh'),
+             bash('omz_downloader --name squeezenet1.1 --precisions FP16 '
+                  '-o /root/inference_engine_cpp_samples_build/intel64/Release/'),
+             bash('omz_converter --name squeezenet1.1 --precisions FP16 '
+                  '-d /root/inference_engine_cpp_samples_build/intel64/Release/'),
+             download_picture('car.png'),
+             bash('umask 0000 && /root/inference_engine_cpp_samples_build/intel64/Release/benchmark_app '
+                  '-m /root/inference_engine_cpp_samples_build/intel64/Release/public/'
+                  'squeezenet1.1/FP16/squeezenet1.1.xml -i /opt/intel/openvino/samples/scripts/car.png -pc -niter 1000 '
+                  '-d HDDL && rm -f /dev/shm/hddl_*'),
+             ], self.test_benchmark_app_cpp_hddl.__name__, **kwargs,
+        )
+
     @pytest.mark.xfail_log(pattern='Error: Download',
                            reason='Network problems when downloading alexnet files')
     def test_hello_classification_cpp_cpu(self, tester, image, install_openvino_dependencies, bash, download_picture):
