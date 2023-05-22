673c690a2d462ddb33d3fd8db628896db513383d
Dariusz Trawinski <Dariusz.Trawinski@intel.com>
2023-05-22 12:16:34 +0200

    unit tests fix

--- utils/arg_parser.py
+++ utils/arg_parser.py
@@ -21,7 +21,7 @@ logger = logging.getLogger(__name__)
 class DockerCIArgumentParser(argparse.ArgumentParser):
     """CLI argument parser for this framework"""
 
-    SUPPORTED_OS: typing.List = ['ubuntu20', 'ubuntu22', 'rhel8']
+    SUPPORTED_OS: typing.List = ['ubuntu18', 'ubuntu20', 'ubuntu22','winserver2019', 'windows20h2', 'rhel8']
 
     def __init__(self, prog: typing.Optional[str] = None, description: typing.Optional[str] = None):
         super().__init__(prog=prog, description=description,
