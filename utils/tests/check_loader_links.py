0b49622ddc6cc3c47df8e4e23c82d80627c02260
Jacek Pawlak <jacek.pawlak@intel.com>
2024-04-09 13:36:35 +0200

    updated copyright year to 2024

--- utils/tests/check_loader_links.py
+++ utils/tests/check_loader_links.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2019-2022 Intel Corporation
+# Copyright (C) 2019-2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 """Utility to check URLs of the release packages embedded inside loader.py"""
 import argparse
